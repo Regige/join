bf54c4fbc2b582c919bf227d88e9ce5c29301e1f
Dennis Ochs <dennisochs17@gmail.com>
2023-10-02 12:38:36 +0200

    Added headline

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function showSignUp() {
+    document.getElementById('sign-up').classList.remove('d-none');
+    document.getElementById('login').classList.add('d-none');
+}
+
+function hideSignUp(){
+    document.getElementById('sign-up').classList.add('d-none');
+    document.getElementById('login').classList.remove('d-none');
+}
