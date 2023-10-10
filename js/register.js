cf2b38bd501f2dfa2bfddb434b3179ae26965b76
Dennis Ochs <dennisochs17@gmail.com>
2023-10-10 13:05:23 +0200

    Update Sign up

---
+++ js/register.js
@@ -0,0 +1,55 @@
+const STORAGE_TOKEN = '8A3U4MK7U3QQZFIE9YT3HJC3MLRAQ8J3J7J4DZ5Y';
+const STORAGE_URL = 'https://remote-storage.developerakademie.org/item';
+
+let users = [];
+
+async function setItem(key, value) {
+    const payload = { key, value, token: STORAGE_TOKEN };
+    return fetch(STORAGE_URL, { method: 'POST', body: JSON.stringify(payload) })
+        .then(res => res.json());
+}
+
+async function getItem(key) {
+    const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
+    return fetch(url).then(res => res.json()).then(res => {
+        // Verbesserter code
+        if (res.data) { 
+            return res.data.value;
+        } throw `Could not find data with key "${key}".`;
+    });
+}
+
+async function init(){
+    loadUsers();
+}
+
+async function loadUsers(){
+    try {
+        users = JSON.parse(await getItem('users'));
+    } catch(e){
+        console.error('Loading error:', e);
+    }
+}
+
+
+async function register() {
+    const registrationForm = document.getElementById('registrationForm');
+    const registerBtn = document.getElementById('registerBtn');
+    const email = document.getElementById('email');
+    const password = document.getElementById('password');
+
+    registerBtn.disabled = true;
+    users.push({
+        email: email.value,
+        password: password.value,
+    });
+    await setItem('users', JSON.stringify(users));
+    resetForm();
+}
+
+function resetForm() {
+    email.value = '';
+    password.value = '';
+    registerBtn.disabled = false;
+}
+
