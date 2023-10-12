28afa6cbbd832713b008bfca82c2447292d97c45
Dennis Ochs <dennisochs17@gmail.com>
2023-10-12 14:06:06 +0200

    Update Log in

--- js/register.js
+++ js/register.js
@@ -34,16 +34,3 @@ function resetForm() {
     registerBtn.disabled = false;
 }
 
-async function login(){
-    let emailLogin = document.getElementById('email');
-    let passwordLogin = document.getElementById('password');
-    let user = users.find(u => u.email == emailLogin.value && u.password == passwordLogin.value);
-    console.log(user);
-    if (user) {
-        console.log('user gefunden');
-        window.location.href = 'summary.html';
-    } else {
-        console.log('Falsche Anmeldeinformationen');
-        document.getElementById("login-error").style.display = "block";
-    }
-}
