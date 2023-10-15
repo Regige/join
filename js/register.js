fc35575b6d08e512da1b9e35023f4d8e9c4837f4
Dennis Ochs <dennisochs17@gmail.com>
2023-10-15 17:11:56 +0200

    Update Log in

--- js/register.js
+++ js/register.js
@@ -12,15 +12,24 @@ async function loadUsers(){
     }
 }
 
+
 async function register() {
     let email = document.getElementById('emailregister');
-    let password = document.getElementById('passwordregister');
+    let password1 = document.getElementById('passwordregister1');
+    let password2 = document.getElementById('passwordregister2');
     let name = document.getElementById('nameregister');
+    let registerBtn = document.getElementById('registerBtn'); // Hinzugefügt
+
+    if (password1.value !== password2.value) {
+        alert('Die beiden Passwörter stimmen nicht überein.');
+        return;
+    }
+
     registerBtn.disabled = true;
     users.push({
         name: name.value,
         email: email.value,
-        password: password.value,
+        password: password1.value,
     });
     await setItem('users', JSON.stringify(users));
     window.location.href = 'index.html';
@@ -28,9 +37,9 @@ async function register() {
 }
 
 function resetForm() {
-    nameregister.value = '';
-    emailregister.value = '';
-    passwordregister.value = '';
-    registerBtn.disabled = false;
+    document.getElementById('nameregister').value = '';
+    document.getElementById('emailregister').value = '';
+    document.getElementById('passwordregister1').value = '';
+    document.getElementById('passwordregister2').value = '';
+    document.getElementById('registerBtn').disabled = false;
 }
-
